201194d579958901c3a64a0776a720080d2b1b32
craniacshencil <134303789+craniacshencil@users.noreply.github.com>
2023-12-29 10:54:26 +0530

    Add ability to play game of 5 rounds
    - Player choice is now taken through prompts and results are displayed using alerts instead of using html elements(body is now empty)
    - 5 rounds can be played to decide the winner
    - round ends when one of the players(computer or user) reaches 5 points

--- consoleLogic.js
+++ consoleLogic.js
@@ -5,35 +5,49 @@ function getComputerChoice(){
 }
 
 function getPlayerChoice(){
-    const playerinput = document.getElementById("playerchoice_input");
-    // const playround = document.querySelector(".playround");
-    return playerinput.value.toLowerCase();
-    // let playerchoice;
-    // playround.addEventListener("click", () => playerchoice = playerinput.value.toLowerCase());
-    // return playerchoice;
+    const player_choice = prompt("Enter your choice (Rock/Paper/Scissors): ");
+    return player_choice.toLowerCase();
 }
 
 function playRound(playerChoice, computerChoice){
     const round_choices = document.querySelector(".round_choices");
-    const round_result = document.querySelector(".round_result");
 
     round_choices.textContent = `You chose ${playerChoice}. Computer chose ${computerChoice}`
     if(playerChoice == computerChoice)
-        round_result.textContent = "Tie. Both of you chose " + playerChoice + " play Again"; 
-
+        return "tie";
+    
     else if((playerChoice == "rock" && computerChoice == "scissors") 
     || (playerChoice == "scissors" && computerChoice == "paper")
     || (playerChoice == "paper" && computerChoice == "rock"))
-        round_result.textContent = "Win!";
-
+        return "win";
+    
+    else if(playerChoice != "rock" && playerChoice != "scissors" &&
+    playerChoice != "paper")
+        return "invalid";
+    
     else
-        round_result.textContent = "Lose";
+        return "lose" 
 }
 
-//Playing round on button click
-const play_button = document.getElementById("play_button");
-play_button.addEventListener("click", () => {
-    const playerChoice = getPlayerChoice();
-    const computerChoice = getComputerChoice();
-    playRound(playerChoice, computerChoice);
-}, false);
+function game(){
+    let playerScore = computerScore = 0;
+    while(playerScore < 5 && computerScore < 5){
+        let playerChoice = getPlayerChoice();
+        let computerChoice = getComputerChoice();
+        let result = playRound(playerChoice, computerChoice);
+    
+        if(result == "win")
+            playerScore++;
+        if(result == "lose")
+            computerScore++;
+
+        alert(`You chose ${playerChoice}. Computer chose ${computerChoice}\n
+        You: ${playerScore} Computer: ${computerScore}`);
+
+        if(playerScore == 5)
+            alert("You won!");
+        else if(computerScore == 5)
+            alert("Computer won!");
+    }
+}
+game();
