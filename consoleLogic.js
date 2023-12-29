504566fc4a594a2e1b53f1751732984c990c8618
craniacshencil <134303789+craniacshencil@users.noreply.github.com>
2023-12-29 09:31:39 +0530

    Add function to play one game
    - getPlayerChoice() function has been implemented to get user input from an html input element
    - playRound() function consists of the logic and prints the result of a match
    - play_button is used to trigger a round after enter a valid input in the html input element

--- consoleLogic.js
+++ consoleLogic.js
@@ -1,5 +1,39 @@
 function getComputerChoice(){
-    const choices = ['Rock', 'Paper', 'Scissors'];
+    const choices = ['rock', 'paper', 'scissors'];
     const choiceIndex = Math.floor(Math.random() * 3);
     return choices[choiceIndex];
 }
+
+function getPlayerChoice(){
+    const playerinput = document.getElementById("playerchoice_input");
+    // const playround = document.querySelector(".playround");
+    return playerinput.value.toLowerCase();
+    // let playerchoice;
+    // playround.addEventListener("click", () => playerchoice = playerinput.value.toLowerCase());
+    // return playerchoice;
+}
+
+function playRound(playerChoice, computerChoice){
+    const round_choices = document.querySelector(".round_choices");
+    const round_result = document.querySelector(".round_result");
+
+    round_choices.textContent = `You chose ${playerChoice}. Computer chose ${computerChoice}`
+    if(playerChoice == computerChoice)
+        round_result.textContent = "Tie. Both of you chose " + playerChoice + " play Again"; 
+
+    else if((playerChoice == "rock" && computerChoice == "scissors") 
+    || (playerChoice == "scissors" && computerChoice == "paper")
+    || (playerChoice == "paper" && computerChoice == "rock"))
+        round_result.textContent = "Win!";
+
+    else
+        round_result.textContent = "Lose";
+}
+
+//Playing round on button click
+const play_button = document.getElementById("play_button");
+play_button.addEventListener("click", () => {
+    const playerChoice = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+    playRound(playerChoice, computerChoice);
+}, false);
